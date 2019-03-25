a1b93dcde84fed647884da41a6d8b19b14b39a60
Arthur Fulgence <a.fulgence@campus-igs-toulouse.fr>
2019-03-25 17:25:06 +0100

    Arthur hanoi TU

--- src/test/java/cfranc/hanoi/DisqueTest.java
+++ src/test/java/cfranc/hanoi/DisqueTest.java
@@ -6,14 +6,66 @@ import org.junit.Test;
 
 public class DisqueTest {
 
+	Disque dSmall = new Disque(1);
+	Disque dMedium = new Disque(2);
+	Disque dTall = new Disque(3);
+
 	@Test
 	public void testCompareTo() {
 		Disque petit = new Disque(1);
 		Disque moyen = new Disque(2);
 		Disque grand = new Disque(3);
+		
 		assertEquals(-1, petit.compareTo(moyen));
 		assertEquals(-1, moyen.compareTo(grand));
+		assertEquals(-1, petit.compareTo(grand));
+		
+		assertEquals(1, moyen.compareTo(petit));
+		assertEquals(1, grand.compareTo(moyen));
+		assertEquals(1, grand.compareTo(petit));
 		//TODO : à compléter
+	}
+
+	@Test
+	public void compareTo_SmallMedium_Negative(){
+		int expected = -1;
+		int actual = dSmall.compareTo(dMedium);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void compareTo_MediumSmall_Positive(){
+		int expected = 1;
+		int actual = dMedium.compareTo(dSmall);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void compareTo_SmallTall_Negative(){
+		int expected = -1;
+		int actual = dSmall.compareTo(dTall);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void compareTo_TallSmall_Positive(){
+		int expected = 1;
+		int actual = dTall.compareTo(dSmall);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void compareTo_MediumTall_Negative(){
+		int expected = -1;
+		int actual = dMedium.compareTo(dTall);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void compareTo_TallMedium_Positive(){
+		int expected = 1;
+		int actual = dTall.compareTo(dMedium);
+		assertEquals(expected, actual);
 	}
 
 }
--- src/test/java/cfranc/hanoi/TourTest.java
+++ src/test/java/cfranc/hanoi/TourTest.java
@@ -5,10 +5,95 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TourTest {
+	
+	Disque dSmall = new Disque(1);
+	Disque dMedium = new Disque(2);
+	Disque dTall = new Disque(3);
+	
+	Tour t = new Tour(3);
 
+	// isEmpty
 	@Test
-	public void test() {
-
+	public void EmptyTour_Small() {
+		assertEquals(true, t.empiler(dSmall));
+	}
+	
+	@Test
+	public void EmptyTour_Medium() {
+		assertEquals(true, t.empiler(dMedium));
+	}
+	
+	@Test
+	public void EmptyTour_Tall() {
+		assertEquals(true, t.empiler(dTall));
+	}
+	
+	// One step
+	@Test
+	public void SmallToMedium() {
+		t.disques.offer(dMedium);
+		assertEquals(true, t.empiler(dSmall));
+	}
+	
+	@Test
+	public void SmallToTall() {
+		t.disques.offer(dTall);
+		assertEquals(true, t.empiler(dSmall));
+	}
+	
+	@Test
+	public void MediumToTall() {
+		t.disques.offer(dTall);
+		assertEquals(true, t.empiler(dMedium));
+	}
+	
+	// Rules respected
+	@Test
+	public void MediumToSmall_False() {
+		t.disques.offer(dSmall);
+		assertEquals(false, t.empiler(dMedium));
+	}
+	
+	@Test
+	public void TallToMedium_False() {
+		t.disques.offer(dMedium);
+		assertEquals(false, t.empiler(dTall));
+	}
+	
+	@Test
+	public void TallToSmall_False() {
+		t.disques.offer(dSmall);
+		assertEquals(false, t.empiler(dTall));
+	}
+	
+	@Test
+	public void TourIsEmpty() {
+		assertEquals(true, t.estVide());
+	}
+	
+	@Test
+	public void TourIsFull() {
+		t.disques.offer(dSmall);
+		t.disques.offer(dMedium);
+		t.disques.offer(dTall);
+		assertEquals(true, t.estPleine());
+	}
+	
+	@Test
+	public void TourIsNotFull() {
+		t.disques.offer(dTall);
+		t.disques.offer(dMedium);
+		t.disques.offer(dSmall);
+		t.depiler();
+		assertEquals(false, t.estPleine());
+	}
+	
+	@Test
+	public void TourSommet() {
+		t.disques.offer(dTall);
+		t.disques.offer(dMedium);
+		t.disques.offer(dSmall);
+		assertEquals(dTall, t.sommet());
 	}
 
 }
